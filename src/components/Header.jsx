b807d1165a8fff0ae07156d33892d15b66980440
steventhomasdev <steventhomasdev@gmail.com>
2024-02-18 11:34:18 -0500

    css updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,25 @@
 import React from "react";
-import { AppBar, Toolbar, Container, Box } from "@mui/material";
-import { styled } from "@mui/system";
+import {AppBar, Toolbar, Container, Box} from "@mui/material";
 import icon from "../assets/logo.svg";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-const Logo = styled(Box)(() => ({
-  width: { xs: "148px", md: "294px" },
-  height: { xs: "32px", md: "32px" },
-}));
 
 export default function Header() {
-  const matches = useMediaQuery("(min-width:600px)");
+
+  const matches = useMediaQuery('(min-width:600px)');
 
   return (
-    <AppBar
-      component="nav"
-      sx={{
-        backgroundColor: "#252F3D",
-        position: matches ? "relative" : "fixed",
-      }}
-    >
+    <AppBar component="nav" sx={{ backgroundColor: "#252F3D",  position: matches ? "relative" : "fixed" }}>
       <Container>
         <Toolbar>
-          <Logo
+          <Box
             component="img"
             alt="The Idea Theorem Logo"
             src={icon}
+            sx={{
+                width: { xs: '148px', md: '294px' },
+                height: { xs: '16px', md: '32px' }
+              }}
           />
         </Toolbar>
       </Container>
